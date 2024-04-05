746a96f6ba1f3fd0d0760aee14f8e70cf016d980
Shivamj4112 <shivamj4112@gmail.com>
2024-04-05 16:26:20 +0530

    Add Swipe to close animation

--- app/src/main/java/com/example/charginganimationsdemo/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/charginganimationsdemo/views/activities/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.b.setOnClickListener {
+        binding.mcvStartAnimation.setOnClickListener {
 
             startActivity(Intent(this,AnimationListActivity::class.java))
         }
